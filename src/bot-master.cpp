676ad72f0b9ba1b62d6147a34990c9e83352d606
novucs <contact@novucs.net>
2017-02-03 17:46:12 +0000

    Switch to CMake

--- src/bot-master.cpp
+++ src/bot-master.cpp
@@ -6,12 +6,13 @@
 
 void bot_master::run() {
 	net.setup();
-	char buffer[4096];
 	net.respawn(BOT_CLASS);
 
 	std::cout << std::endl << "===========================" << std::endl;
 	std::cout << std::endl << "     Master bot loaded     " << std::endl;
 	std::cout << std::endl << "===========================" << std::endl;
+
+	char buffer[4096];
 
 	for (;;) {
 		if (!net.receive(net.get_client(), buffer, sizeof(buffer))) {
@@ -25,5 +26,6 @@ void bot_master::run() {
 
 		ships = net.load_ships(buffer);
 	}
+
 	net.close();
 }
